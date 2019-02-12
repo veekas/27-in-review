ec16d5be56b9dfc781905480d8c7b421cc2f0be7
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2019-02-11 21:23:07 -0800

    meta changes and lint

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Veekas Shrivastava - 27 In Review`,
+    description: `A recap of Veekas Shrivastava's 27th year alive.`,
+    author: `@veekas`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,11 +24,9 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`, // relative to site root
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    'gatsby-plugin-offline',
   ],
 }
